bbb01f1e1323723c8d8011b3b59fa9ad6bcb198c
José Silvio <josesilvio.bs@gmail.com>
2023-12-16 08:38:51 -0300

    Pequenas correções

--- app/src/main/java/com/example/gerenciador_contratos/features/todosContratos/TodosContratosActivity.kt
+++ app/src/main/java/com/example/gerenciador_contratos/features/todosContratos/TodosContratosActivity.kt
@@ -2,21 +2,13 @@ package com.example.gerenciador_contratos.features.todosContratos
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.example.gerenciador_contratos.R
-import com.example.gerenciador_contratos.network.RetrofitClient
-import com.example.gerenciador_contratos.data.models.Contrato
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+
 
 class TodosContratosActivity : AppCompatActivity() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
 
 }
